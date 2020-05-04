299f05d03e76a1daea3158db26429a349fddd11e
wangy <468077240@qq.com>
2020-05-04 11:28:42 +0800

    merge RealNVP to VAE

--- ais_vae/vae.py
+++ ais_vae/vae.py
@@ -4,6 +4,7 @@ import tensorflow_probability as tfp
 tf = tensorflow.compat.v1 
 tf.disable_v2_behavior()
 tfd = tfp.distributions
+tfb = tfp.bijectors
 
 
 class VAE:
@@ -13,6 +14,15 @@ class VAE:
         self.encoder_layers = encoder_layers
         self.decoder_layers = decoder_layers
         self.batch_mlp = self.__batch_mlp
+
+        couple1 = tfb.RealNVP(num_masked=int(z_dim / 2),
+                        shift_and_log_scale_fn=tfb.real_nvp_default_template(
+                            hidden_layers=[z_dim, z_dim, z_dim]))
+        remask1 = tfb.Permute(permutation=list(range(0, z_dim))[::-1])
+        couple2 = tfb.RealNVP(num_masked=int(z_dim / 2),
+                              shift_and_log_scale_fn=tfb.real_nvp_default_template(
+                                  hidden_layers=[z_dim, z_dim, z_dim]))
+        self.flow_bijector = tfb.Chain( [couple2, remask1, couple1])
 
         self.Loss, self.ELBO, self.pri_dist =\
             self.__graph_create( self.x_op, z_dim, encoder_layers, decoder_layers, dtype)
@@ -38,12 +48,18 @@ class VAE:
     def __graph_create( self, x, z_dim, encoder_layers, decoder_layers, dtype):
         with tf.variable_scope( 'vae_graph', reuse=tf.AUTO_REUSE):
             mean, sigma = tf.split(  self.__batch_mlp( x, encoder_layers, 'encoder'), num_or_size_splits=2, axis=-1)
+
+            self.RealNVP = tfd.TransformedDistribution(
+                       distribution = tfd.MultivariateNormalDiag(loc=mean, scale_diag=sigma),
+                       bijector=self.flow_bijector)
+                       
             pri_dist = tfd.MultivariateNormalDiag( 
                         loc=tf.constant( 0., shape=[z_dim], dtype=dtype), 
                         scale_diag=tf.constant( 1., shape=[z_dim], dtype=dtype))
 
             noise = tf.to_double( pri_dist.sample( sample_shape=tf.shape(x)[0]))
             z = mean + tf.multiply( sigma, noise)
+            z = self.RealNVP.bijector.forward(z)
             y_mn, y_sd = tf.split(  self.__batch_mlp( z, decoder_layers, 'decoder'), num_or_size_splits=2, axis=-1)
             y_sd = tf.sigmoid( y_sd)
 
@@ -53,7 +69,7 @@ class VAE:
             p_z_x = tfd.MultivariateNormalDiag( y_mn, y_sd)
             log_p_z_x = p_z_x.log_prob( x)
 
-            ELBO = -KL_pri_posteriori + log_p_z_x
+            ELBO = -KL_pri_posteriori + log_p_z_x - tf.reduce_mean( self.RealNVP.bijector.inverse_log_det_jacobian( z, event_ndims=1))
             Loss = -tf.reduce_mean( ELBO)
 
         return Loss, ELBO, pri_dist
